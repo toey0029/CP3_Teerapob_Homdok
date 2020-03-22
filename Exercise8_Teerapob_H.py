9d7ee324dbdc9c300d6c07797c8fc02981edef3f
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-22 21:58:30 +0700

    Create Exercise8_Teerapob_H.py

---
+++ Exercise8_Teerapob_H.py
@@ -0,0 +1,12 @@
+user = input("username : ")
+password = input("password : ")
+if user == ("toey") and password == ("1234"):
+    print("Wellcome Toey")
+    print("---100Shop---")
+    print("1.Banana      10 Baht")
+    print("2.Water        8 Baht")
+    print("3.Chocolate   20 Baht")
+    print("4.")
+    elif
+else:
+    print("Login Fail...")
