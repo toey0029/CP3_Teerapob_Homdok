656afb5e71af3f63d18c8bd7557d64ca6e647b72
toey0029 <57355037+toey0029@users.noreply.github.com>
2020-03-27 12:06:42 +0700

    Create Lecture 71_Teerapob_H.py

---
+++ Lecture 71_Teerapob_H.py
@@ -0,0 +1,23 @@
+menuList = []
+priceList = []
+
+def showBill():
+    print("My Food")
+    totalprice = 0
+    for number in range(len(menuList)):
+        print(menuList[number],priceList[number])
+        totalprice += int(priceList[number])
+    print("Total : ",totalprice)
+
+while True:
+    menuName = input("Enter your menu : ")
+    if(menuName.lower() == "exit"):
+        break
+    else:
+        menuPrice = input("Price : ")
+        menuList.append(menuName)
+        priceList.append(int(menuPrice))
+
+
+showBill()
+
